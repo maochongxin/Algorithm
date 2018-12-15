5f6615f9848ee0e23d00347acc3701bbffeea064
maochongxin <maochongxin@gmail.com>
2018-12-15 21:00:38 +0800

    2018_12_15

--- LeetCode/验证外星语词典/main.cpp
+++ LeetCode/验证外星语词典/main.cpp
@@ -23,16 +23,16 @@ public:
     }
     
     bool judge(string s1,string s2,map<char,int>& m) {
-    for (int i = 0; i < min(s1.length(),s2.length()); i++) {
-        if (m[s1[i]] == m[s2[i]]) {
-            continue;
-        } else if (m[s1[i]] < m[s2[i]]){
-            return true;
-        } else {
-            return false;
+        for (int i = 0; i < min(s1.length(),s2.length()); i++) {
+            if (m[s1[i]] == m[s2[i]]) {
+                continue;
+            } else if (m[s1[i]] < m[s2[i]]){
+                return true;
+            } else {
+                return false;
+            }
         }
-    }
-    return s1.length() < s2.length();
+        return s1.length() < s2.length();
     }
 
     
