b6ef4c8217c1597c6cdfe40a76ce1868a690eb84
maochongxin <maochongxin@gmail.com>
2019-01-02 19:04:23 +0800

    2019_1_2

--- TinyThreadPool/TinyThreadPool/main.cpp
+++ TinyThreadPool/TinyThreadPool/main.cpp
@@ -17,33 +17,32 @@ int main(){
      }
      tp::thread_guard<std::thread> tg(v);*/
     std::mutex mtx;
-    try{
+    try {
         thread_pool tp;
         std::vector<std::future<int>> v;
         std::vector<std::future<void>> v1;
-        for (int i = 0; i != 10; ++i){
+        for (int i = 0; i != 10; ++i) {
             auto ans = tp.submit([](int answer) { return answer; }, i);
             v.push_back(std::move(ans));
         }
-        for (int i = 0; i != 5; ++i){
-            auto ans = tp.submit([&mtx](const std::string& str1, const std::string& str2){
+        for (int i = 0; i != 5; ++i) {
+            auto ans = tp.submit([&mtx](const std::string& str1, const std::string& str2) {
                 std::lock_guard<std::mutex> lg(mtx);
                 std::cout << (str1 + str2) << std::endl;
                 return;
             }, "hello ", "world");
             v1.push_back(std::move(ans));
         }
-        for (int i = 0; i != v.size(); ++i){
+        for (int i = 0; i != v.size(); ++i) {
             std::lock_guard<std::mutex> lg(mtx);
             std::cout << v[i].get() << std::endl;
         }
-        for (int i = 0; i != v1.size(); ++i){
+        for (int i = 0; i != v1.size(); ++i) {
             v1[i].get();
         }
-    }catch (std::exception& e){
+    } catch (std::exception& e) {
         std::cout << e.what() << std::endl;
     }
-    
-    system("pause");
+
     return 0;
 }
