cdfd1dfc85b30932b630d1590c1d4cc02b1b00f1
maochongxin <maochongxin@gmail.com>
2019-01-06 23:02:27 +0800

    2019_1_6

---
+++ LeetCode/Evaluate Reverse Polish Notation/main.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+        for (string str : tokens) {
+            if (!isSymble(str)) {
+                s.push(atoi(str.c_str()));
+            } else {
+                int val1 = s.top();
+                s.pop();
+                int val2 = s.top();
+                s.pop();
+                int res;
+                if (str == "+") {
+                    res = val1 + val2;
+                } else if (str == "-") {
+                    res = val2 - val1;
+                } else if (str == "*") {
+                    res = val1 * val2;
+                } else if (str == "/") {
+                    res = val2 / val1;
+                }
+                
+                s.push(res);
+            }
+        }
+        return s.top();
+    }
+private:
+    bool isSymble(string str) {
+        return str == "+" || str == "-" || str == "*" || str == "/";
+    }
+};
